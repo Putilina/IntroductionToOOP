01be400417099e460d4317fc14fcd4cfb219e570
Pascall <Pascall@HOME-PC>
2024-06-04 21:28:42 +0300

    OOP

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -5,8 +5,56 @@ class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
 
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	//   Constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Construcror:\t\t" << this << endl;
+	}
+
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//  Methods:
+	void print()const
+	{
+		cout << "X=" << x << "\tY=" << y << endl;
+	}
 };
+
 
 //#define STRUCT_POINT
 void main()
@@ -29,6 +77,16 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 
  #endif // STRUCT_POINT
+
 	Point A;
-	cout << A.x << "\t" << A.y << endl;
+	//A.set_x(2);
+	//A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B = 5;   // Single-Argument Constructor
+	B.print();
+
+	Point C(2, 3);
+	C.print();
 }
